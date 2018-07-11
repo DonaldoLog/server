7aea3724a445c9dc7531fd28254f874765d31d5e
Donaldo Lopez <donaldolog@gmail.com>
2018-07-11 12:41:11 -0500

    login en myssoserver

--- app/MySSOServer.php
+++ app/MySSOServer.php
@@ -1,22 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: awnali
- * Date: 6/14/17
- * Time: 3:57 PM
- */
 
 namespace App;
-
-
 use Jasny\SSO\Server;
 use Jasny\ValidationResult;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 
 class MySSOServer extends Server
 {
-
     /**
      * Registered brokers
      * @var array
@@ -26,7 +18,6 @@ class MySSOServer extends Server
         'Greg' => ['secret'=>'7pypoox2pc'],
         'Julias' => ['secret'=>'ceda63kmhp']
     ];
-
     /**
      * Get the API secret of a broker and other info
      *
@@ -38,6 +29,22 @@ class MySSOServer extends Server
         return isset(self::$brokers[$brokerId]) ? self::$brokers[$brokerId] : null;
     }
 
+    public function login()
+    {
+        $this->startBrokerSession();
+        if (empty($_GET['username'])) $this->fail("No username specified", 400);
+        if (empty($_GET['password'])) $this->fail("No password specified", 400);
+
+
+        $validation = $this->authenticate($_GET['username'], $_GET['password']);
+
+        if ($validation->failed()) {
+            return $this->fail($validation->getError(), 400);
+        }
+
+        $this->setSessionData('sso_user', $_GET['username']);
+        $this->userInfo();
+    }
     /**
      * Authenticate using user credentials
      *
@@ -50,19 +57,23 @@ class MySSOServer extends Server
         if (!isset($username)) {
             return ValidationResult::error("username isn't set");
         }
-
         if (!isset($password)) {
             return ValidationResult::error("password isn't set");
         }
+
+         // dd(Auth::attempt(['email' => 'fgeneral2@fiscaliaveracruz.gob.mx', 'password' => 'pruebauipj']));
+
+        // $this->guard()->attempt(
+        //     $this->credentials($request), $request->filled('remember')
+        // );
+        // dd(Auth::attempt(['email' => $username, 'password' => $password]));
+
 
         if(Auth::attempt(['email' => $username, 'password' => $password])){
             return ValidationResult::success();
         }
         return ValidationResult::error("can't find user");
-
     }
-
-
     /**
      * Get the user information
      *
